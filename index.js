3a99f9ad1573a50cc6d8a555e2dc4c46b3784963
Daniel Young <Danyoung91@gmail.com>
2022-11-09 10:58:05 -0500

    commit tree c7ccbcace854ba69f826d392cad2c320f9c2c730

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const newTutorials = [];
+  for (let title of tutorials) {
+    const newTitle = title.split(" ");
+    for (let i = 0; i < newTitle.length; i++) {
+      newTitle[i] = newTitle[i].charAt(0).toUpperCase() + newTitle[i].slice(1);
+    }
+    const bestTitle = newTitle.join(" ");
+    newTutorials.push(bestTitle);
+  }
+  console.log(newTutorials);
+  return newTutorials;
+
+  // return (titles = titles.map((title) => {
+  //   title = title
+  //     .split(" ")
+  //     .map((word) => {
+  //       word = word.charAt(0).toUpperCase() + word.slice(1);
+  //       console.log(word);
+  //     })
+  //     .join(" ");
+  // }));
+
+  // titles.forEach((title) => {
+  //   title = title
+  //     .split(" ")
+  //     .map((word) => {
+  //       word = word.charAt(0).toUpperCase() + word.slice(1);
+  //       console.log(word);
+  //     })
+  //     .join(" ");
+  // });
+  // return titles;
+};
